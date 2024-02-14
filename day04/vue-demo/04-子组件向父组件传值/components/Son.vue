0cf2a10b2018d5b5adb2f00cdb110dee792d8d26
hanjunan <1401224905@qq.com>
2024-02-14 20:48:20 +0800

    day04-props接收任意类型、任意数量的自定义属性

--- day04/vue-demo/04-子组件向父组件传值/components/Son.vue
+++ day04/vue-demo/04-子组件向父组件传值/components/Son.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="son" style="border: 3px solid #000; margin: 10px">
-    我是Son组件 {{ title }}
-    <button>修改title</button>
+  <div class="son" style="border:3px solid #000;margin:10px">
+    <button @click="change">修改Title</button>
+    我是Son组件
+    父组件传递过来的属性: {{ title }}
   </div>
 </template>
 
@@ -10,9 +11,15 @@ export default {
   name: 'Son-Child',
   props: ['title'],
   methods: {
-  },
+    change() {
+      // this.title = '你好'
+      // 父组件传递过来的props,子组件无法直接修改
+      this.$emit("changeTitle",' 学前端找道长')
+    }
+  }
 }
 </script>
 
 <style>
+
 </style>
--- day04/vue-demo/src/App.vue
+++ day04/vue-demo/src/App.vue
@@ -1,37 +1,33 @@
 <template>
-  <div class="app" style="border: 3px solid #000; margin: 10px">
-    我是APP组件 --- {{ myTitle }}
-    <Son @changeTitle="change" :title="myTitle"></Son>
+  <!-- props可以接收任意类型 任意数量的自定义属性 -->
+  <div class="app">
+    <UserInfo
+      :username="username"
+      :age="age"
+      :isSingle="isSingle"
+      :car="car"
+      :hobby="hobby"
+    ></UserInfo>
   </div>
 </template>
 
 <script>
-/**
- * 父向子传递数据
- * 1. 在App.vue中给子组件添加自定义属性
- * 2. 在Son.vue中通过props接收自定义属性传递过来的数据
- * 
- * 子向父传递数据
- * 1. 在Son.vue中,使用$emit传递数据
- * 2. 在App.vue中,使用自定义事件来接收数据
- */
-import Son from './components/Son.vue'
+import UserInfo from './components/UserInfo.vue'
 export default {
-  name: 'App',
   data() {
     return {
-      myTitle: '学前端，就来黑马程序员',
+      username: '小帅',
+      age: 28,
+      isSingle: true,
+      car: {
+        brand: '宝马',
+      },
+      hobby: ['篮球', '足球', '羽毛球'],
     }
   },
   components: {
-    Son,
+    UserInfo,
   },
-  methods: {
-    change(val) {
-      // console.log("父组件中的chaneg触发了..",val);
-      this.myTitle = val;
-    }
-  }
 }
 </script>
 
