f70a75213712095fc6e9b9ebf4609eb1fc291a00
hanjunan <1401224905@qq.com>
2024-02-14 20:20:02 +0800

    day04-子向父传值

--- day04/vue-demo/src/App.vue
+++ day04/vue-demo/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app" style="border: 3px solid #000; margin: 10px">
     我是APP组件 --- {{ myTitle }}
-    <Son :title="myTitle"></Son>
+    <Son @changeTitle="change" :title="myTitle"></Son>
   </div>
 </template>
 
@@ -10,6 +10,10 @@
  * 父向子传递数据
  * 1. 在App.vue中给子组件添加自定义属性
  * 2. 在Son.vue中通过props接收自定义属性传递过来的数据
+ * 
+ * 子向父传递数据
+ * 1. 在Son.vue中,使用$emit传递数据
+ * 2. 在App.vue中,使用自定义事件来接收数据
  */
 import Son from './components/Son.vue'
 export default {
@@ -22,6 +26,12 @@ export default {
   components: {
     Son,
   },
+  methods: {
+    change(val) {
+      // console.log("父组件中的chaneg触发了..",val);
+      this.myTitle = val;
+    }
+  }
 }
 </script>
 
--- day04/vue-demo/src/components/Son.vue
+++ day04/vue-demo/src/components/Son.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="son" style="border:3px solid #000;margin:10px">
+    <button @click="change">修改Title</button>
     我是Son组件
     父组件传递过来的属性: {{ title }}
   </div>
@@ -8,7 +9,14 @@
 <script>
 export default {
   name: 'Son-Child',
-  props: ['title']
+  props: ['title'],
+  methods: {
+    change() {
+      // this.title = '你好'
+      // 父组件传递过来的props,子组件无法直接修改
+      this.$emit("changeTitle",' 学前端找道长')
+    }
+  }
 }
 </script>
 
