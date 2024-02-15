2ecfb009e882272b7808fa4483803e6a2cfa4ce7
hanjunan <1401224905@qq.com>
2024-02-15 20:30:43 +0800

    day04-小黑记事本-初始化

--- day04/vue-demo/src/App.vue
+++ day04/vue-demo/src/App.vue
@@ -1,22 +1,39 @@
 <template>
-  <div class="app">
-    <BaseProgress :w="width"></BaseProgress>
-  </div>
+  <!-- 主体区域 -->
+  <section id="app">
+    <todo-header></todo-header>
+    <todo-main :list="list"></todo-main>
+    <todo-footer></todo-footer>
+  </section>
 </template>
 
 <script>
-import BaseProgress from './components/BaseProgress.vue'
+/**
+ * 目标1: 拆分组件
+ * 目标2: 渲染数据
+ *  2.1 明确数据list定义在App.vue中
+ *  2.2 父向子传值, 设置自定义属性
+ *  2.3 子组件使用props来接收数据(类型校验)
+ *  2.4 子组件中使用v-for来渲染数据
+ * 
+ */
+import TodoHeader from './components/TodoHeader.vue'
+import TodoMain from './components/TodoMain.vue'
+import TodoFooter from './components/TodoFooter.vue'
 export default {
-  data() {
+  components: { TodoHeader, TodoMain, TodoFooter },
+  data () {
     return {
-      width: 30,
+      list: [
+        {id: 1, name: '跑步十公里'},
+        {id: 2, name: '打球一整天'},
+        {id: 3, name: '吃饭一公斤'}
+      ]
     }
-  },
-  components: {
-    BaseProgress,
-  },
+  }
 }
 </script>
 
 <style>
+
 </style>
---
+++ day04/vue-demo/src/components/TodoMain.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- 列表区域 -->
+  <section class="main">
+      <ul class="todo-list">
+        <li class="todo" v-for="(item, index) in list" :key="item.id">
+          <div class="view">
+            <span class="index">{{ index }}.</span> <label>{{ item.name }}</label>
+            <button class="destroy"></button>
+          </div>
+        </li>
+      </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        list: {
+            type: Array,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
