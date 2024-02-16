1b96584ac8d04e30062f942b37a84f61b7d11b84
hanjunan <1401224905@qq.com>
2024-02-16 20:42:27 +0800

    day04-异步更新视图

--- day04/vue-demo/src/App.vue
+++ day04/vue-demo/src/App.vue
@@ -1,24 +1,39 @@
 <template>
   <div class="app">
-    <div class="base-chart-box">
-      这是一个捣乱的盒子
+    <div v-if="isShowEdit">
+      <input type="text" v-model="editValue" ref="inp" />
+      <button>确认</button>
     </div>
-    <BaseChart></BaseChart>
+    <div v-else>
+      <span>{{ title }}</span>
+      <button @click="editFn">编辑</button>
+    </div>
   </div>
 </template>
 
 <script>
-import BaseChart from './components/BaseChart.vue'
 export default {
-  components:{
-    BaseChart
-  }
+  data() {
+    return {
+      title: '大标题',
+      isShowEdit: false,
+      editValue: '',
+    }
+  },
+  methods: {
+    editFn() {
+      // 1.显示文本框
+      // 在Vue中数据变化视图会更新,关键点: 视图的更新是异步任务
+      // DOM更新是异步的
+      this.isShowEdit = true
+      // 2.让文本框聚焦
+      this.$nextTick(() => {
+        this.$refs.inp.focus()
+      })
+    },
+  },
 }
 </script>
 
 <style>
-.base-chart-box {
-  width: 300px;
-  height: 200px;
-}
 </style>
