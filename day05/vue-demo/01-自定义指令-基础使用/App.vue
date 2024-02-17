959a919814edb199fd43dfa2fab73ace4e1c1385
hanjunan <1401224905@qq.com>
2024-02-18 00:21:31 +0800

    day05-指令的值

---
+++ day05/vue-demo/01-自定义指令-基础使用/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div >
+    <h1>自定义指令</h1>
+    <input v-focus ref="inp" type="text">
+  </div>
+</template>
+
+<script>
+// 局部注册指令
+
+/**
+ * 自定义指令: 实现自动聚焦
+ */
+export default {
+  directives: {
+    // 指令名: 配置对象
+    focus: {
+      inserted(el) {
+        el.focus();
+      }
+    }
+  }
+  // mounted() {
+  //   this.$refs.inp.focus()
+  // }
+}
+</script>
+
+<style>
+
+</style>
--- day05/vue-demo/src/App.vue
+++ day05/vue-demo/src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div >
-    <h1>自定义指令</h1>
-    <input v-focus ref="inp" type="text">
+  <div>
+    <h1 v-color="color1">指令的值1测试</h1>
+    <h1 v-color="color2">指令的值2测试</h1>
   </div>
 </template>
 
 <script>
-// 局部注册指令
-
 /**
- * 自定义指令: 实现自动聚焦
+ * 总结:
+ * 1.通过指令的值相关语法,可以应对更复杂指令封装的场景
+ * 2.指令值得语法:
+ *  2.1 v-指令名 = "指令值",通过等号可以绑定指令的值
+ *  2.2 通过binding.value可以拿到指令的值
+ *  2.3 通过update钩子,可以监听指令值得变化,进行DOM更新操作
+ * 
  */
 export default {
+  // 注册局部指令
   directives: {
-    // 指令名: 配置对象
-    focus: {
-      inserted(el) {
-        el.focus();
+    color: {
+      // 参数1: 指令绑定的元素对象
+      // 参数2: 指令相关的值
+      // inserted: 会在元素插入到DOM时执行
+      // update: 会在指令的值更新时自动执行
+      inserted(el, binding) {
+        // console.log("el = ",el);
+        // console.log("binding = ",binding);
+        el.style.color = binding.value
+      },
+      update(el, binding) {
+        console.log("update el = ",el);
+        console.log("update binding = ",binding);
+        el.style.color = binding.value
       }
     }
-  }
-  // mounted() {
-  //   this.$refs.inp.focus()
-  // }
+  },
+  data () {
+    return {
+      color1: 'red',
+      color2: 'orange'
+    }
+  },
 }
 </script>
 
