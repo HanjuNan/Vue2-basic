9c5b84f940ba3a35f7625c0b6889a15da44342b4
hanjunan <1401224905@qq.com>
2024-02-19 17:04:38 +0800

    day05-商品列表案例-完成

--- day05/vue-demo/src/components/MyTable.vue
+++ day05/vue-demo/src/components/MyTable.vue
@@ -2,43 +2,40 @@
   <table class="my-table">
     <thead>
       <tr>
-        <th>编号</th>
+        <slot name="header"></slot>
+        <!-- <th>编号</th>
         <th>名称</th>
         <th>图片</th>
-        <th width="100px">标签</th>
+        <th width="100px">标签</th> -->
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>1</td>
-        <td>梨皮朱泥三绝清代小品壶经典款紫砂壶</td>
+      <tr v-for="(item, index) in data" :key="item.id">
+        <slot name="body" :item="item" :index="index"></slot>
+        <!-- <td>{{ index + 1 }}</td>
+        <td>{{ item.name }}</td>
         <td>
           <img
-            src="https://yanxuan-item.nosdn.127.net/f8c37ffa41ab1eb84bff499e1f6acfc7.jpg"
+            :src="item.picture"
           />
         </td>
         <td>
-          标签组件
-        </td>
+          {{ item.tag }}
+        </td> -->
       </tr>
-      <tr>
-        <td>2</td>
-        <td>全防水HABU旋钮牛皮户外徒步鞋山宁泰抗菌</td>
-        <td>
-          <img
-            src="https://yanxuan-item.nosdn.127.net/221317c85274a188174352474b859d7b.jpg"
-          />
-        </td>
-        <td>
-          标签组件
-        </td>
-      </tr>
+      
     </tbody>
   </table>
 </template>
 
 <script>
 export default {
+  props: {
+    data: {
+      type: Array,
+      required: true
+    }
+  }
 }
 </script>
 
