91f4efb730a7d537eecfcce4a3dabfeea1807d2e
hanjunan <1401224905@qq.com>
2024-02-22 14:26:04 +0800

    day06-处理404页面

--- day06/vue-demo/src/router/index.js
+++ day06/vue-demo/src/router/index.js
@@ -1,5 +1,6 @@
 import Home from '@/views/Home'
 import Search from '@/views/Search'
+import NotFind from "@/views/NotFind"
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
@@ -8,9 +9,10 @@ const router = new VueRouter({
   routes: [
     // path: 匹配用户访问的路径
     // redirect: 重定向到指定的路径
-    {path: '/', redirect: '/home'},
+    { path: '/', redirect: '/home'},
     { path: '/home', component: Home },
-    { path: '/search/:words?', component: Search }
+    { path: '/search/:words?', component: Search },
+    { path: '*', component: NotFind }
   ]
 })
 
---
+++ day06/vue-demo/src/views/NotFind.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img src="https://img2.baidu.com/it/u=1654642651,3823686829&fm=253&fmt=auto&app=138&f=JPEG?w=474&h=313" />
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFind'
+}
+</script>
+
+<style scoped lang="less">
+div {
+    text-align: center;
+    img {
+        margin: 0 auto;
+    }
+}
+</style>
