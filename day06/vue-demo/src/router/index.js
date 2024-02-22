e71a4a9fb7d85a727f7127c04018d45cd065afa9
hanjunan <1401224905@qq.com>
2024-02-22 23:09:47 +0800

    day06-面经案例-分析

--- day06/vue-demo/src/router/index.js
+++ day06/vue-demo/src/router/index.js
@@ -1,20 +1,9 @@
-import Home from '@/views/Home'
-import Search from '@/views/Search'
-import NotFind from "@/views/NotFind"
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  mode: "history",
-  routes: [
-    // path: 匹配用户访问的路径
-    // redirect: 重定向到指定的路径
-    { path: '/', redirect: '/home'},
-    { path: '/home', component: Home },
-    { name: 'search', path: '/search/:words?', component: Search },
-    {  path: '*', component: NotFind }
-  ]
+  routes: []
 })
 
 export default router
