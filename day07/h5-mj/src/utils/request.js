76a178c52b52f61d07a7a7aa22bb7d5458700501
hanjunan <1401224905@qq.com>
2024-02-26 01:46:09 +0800

    day08-封装request

---
+++ day07/h5-mj/src/utils/request.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+// 1.不方便
+// 2.修改了axios这个对象,以后项目中所有用到的axios都被修改了
+// 建议: 新建一个axios对象来配置
+// axios.defaults.baseURL = 'http://localhost:3000'
+// axios.defaults.timeout = 5000
+
+// 创建一个axios对象,功能和axios完全一样,但起到了隔离的作用
+const instance = axios.create({
+  baseURL: 'http://interview-api-t.itheima.net/h5/',
+  timeout: 5000
+})
+
+// 直接复制过来是给axios再添加拦截器
+// 需要修改为instance
+// 添加请求拦截器
+instance.interceptors.request.use(function (config) {
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
+
+// 响应拦截器
+instance.interceptors.response.use(function (response) {
+  return response
+}, function (error) {
+  return Promise.reject(error)
+})
+
+export default instance
