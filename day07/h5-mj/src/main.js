c0eea3822909fb5b69b928daf4d3f50fd62e6285
hanjunan <1401224905@qq.com>
2024-02-25 11:55:11 +0800

    day07-项目引入VantUI

--- day07/h5-mj/src/main.js
+++ day07/h5-mj/src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
 // 全自动解决ESLint的错误
+
+Vue.use(Vant)
 
 new Vue({
   router,
