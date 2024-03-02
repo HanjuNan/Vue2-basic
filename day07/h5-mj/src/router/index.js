075f3b40222bf43329ad296dab80135c38426823
hanjunan <1401224905@qq.com>
2024-03-02 19:09:52 +0800

    day08-使用全局前置守卫进行页面拦截

--- day07/h5-mj/src/router/index.js
+++ day07/h5-mj/src/router/index.js
@@ -8,7 +8,8 @@ import Collect from '@/views/Collect.vue'
 import Like from '@/views/Like.vue'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import { getToken } from '@/utils/storage'
+import { Toast } from 'vant'
 Vue.use(VueRouter)
 
 const routes = [
@@ -52,9 +53,11 @@ const routes = [
 const router = new VueRouter({
   routes
 })
-
+// 白名单列表
+const whiteList = ['/login', '/register', '/404']
 // 导航守卫 => 全局前置守卫
 router.beforeEach((to, from, next) => {
+  // 执行时机: 每次页面跳转都会出发该函数
   // 只要注册了全局前置守卫,路由会立即瘫痪
   // 必须调用next()方法来放行
   // 全局前置守卫的回调函数的参数
@@ -63,6 +66,24 @@ router.beforeEach((to, from, next) => {
   // 参数3: next 放行函数
   // next("路径") 表示强制跳转到指定路径, next(false) 表示不放行
 
+  // 1.判断是否有token
+  const token = getToken()
+  if (token) {
+    // 1.存在token
+    // 直接放行
+    next()
+  } else {
+    // 2.不存在token
+    // 判断是否在白名单
+    // 如果要跳转到的路径刚好是login或者register,则放行
+    if (whiteList.includes(to.path)) {
+      next()
+    } else {
+      // 如果不是login或者register,则强制跳转到login
+      Toast.fail('您还未登录,请先登录')
+      next('/login')
+    }
+  }
 })
 
 export default router
