4360b5af1292d1481b1a5bc446f40ff009bb5898
hanjunan <1401224905@qq.com>
2024-03-02 18:24:15 +0800

    day08-导航守卫-简介

--- day07/h5-mj/src/router/index.js
+++ day07/h5-mj/src/router/index.js
@@ -53,4 +53,16 @@ const router = new VueRouter({
   routes
 })
 
+// 导航守卫 => 全局前置守卫
+router.beforeEach((to, from, next) => {
+  // 只要注册了全局前置守卫,路由会立即瘫痪
+  // 必须调用next()方法来放行
+  // 全局前置守卫的回调函数的参数
+  // 参数1: to 去哪里
+  // 参数2: from 从哪里来
+  // 参数3: next 放行函数
+  // next("路径") 表示强制跳转到指定路径, next(false) 表示不放行
+
+})
+
 export default router
