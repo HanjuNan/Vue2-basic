f5443076c8c59b734e0ca84a31d8d69dd6e9c8d5
hanjunan <1401224905@qq.com>
2024-03-03 09:41:51 +0800

    day08-首尔-封装ArticleItem组件和进行全局注册

--- day07/h5-mj/src/main.js
+++ day07/h5-mj/src/main.js
@@ -4,11 +4,15 @@ import router from './router'
 import './utils/vant-ui'
 // import Vant from 'vant'
 // import 'vant/lib/index.css'
-
+// 引入需要全局注册的组件 ArticleItem
+import ArticleItem from '@/components/ArticleItem.vue'
 Vue.config.productionTip = false
 // 全自动解决ESLint的错误
 
 // Vue.use(Vant)
+
+// 注册全局组件
+Vue.component('ArticleItem', ArticleItem)
 
 new Vue({
   router,
