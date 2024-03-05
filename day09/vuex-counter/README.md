4168ebb712eff1728679b5893b75d35f76977660
hanjunan <1401224905@qq.com>
2024-03-06 00:16:07 +0800

    day09-state辅助函数-mapState的使用及原理

--- day09/vuex-counter/README.md
+++ day09/vuex-counter/README.md
@@ -30,8 +30,20 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 State提供唯一的公共数据源,所有共享的数据都放到Store中的
 在state对象中可以添加我们需要共享的数据
 区别:
-1.data是组件自己的数据
-2.state是所有组件共享的数据
+1.1 data是组件自己的数据
+1.2 state是所有组件共享的数据
 
-
-
+2.使用数据
+2.1 通过store直接访问
+2.2 通过辅助函数(简化)
+mapState是辅助函数,帮助我们把store中的数据自动映射到组件的计算属性中
+正常来说是我们需要自己定义一个计算属性
+computed: {
+    count() {
+        return this.$store.state.count
+    }
+}
+但是如果通过mapState辅助函数我们只需要
+computed: {
+    ...mapState(['count'])
+}
--- day09/vuex-counter/src/components/Right.vue
+++ day09/vuex-counter/src/components/Right.vue
@@ -3,14 +3,26 @@
     <h3>Right 组件</h3>
     <hr>
 
-    <p>count 值：{{ $store.state.count }}</p>
+    <p>count 值：{{ count }}</p>
     <button class="btn btn-warning">-1</button>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+// console.log('mapState = ', mapState(['count']))
+// mapState的返回值是对象 ...mapState()对对象进行解构赋值,将对应的函数取出来
+// 大白话理解辅助函数: 就是vuex帮我们写好了计算属性,放到对象中返回给我们了.
+// 我们之后只需要将其在computed中展开即可
 export default {
-  name: 'MyRight'
+  name: 'MyRight',
+  computed: {
+    // count () {
+    //   return this.$store.state.count
+    // }
+    // mapState是一个函数
+    ...mapState(['count'])
+  }
 }
 </script>
 
